3561a2f654f5cb05cc2b95ee3cc345d8a88d25f0
Neda <neda1984saberi@gmail.com>
2023-09-03 23:38:10 +0200

    home-v1 initial layout

--- src/pages/home-v2/Home-v2.tsx
+++ src/pages/home-v2/Home-v2.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
-export default function HomeV2() {
+export default function HomeV2(){
   return (
-    <div className="first-view">
-      <div className="first-view-content">
-        <h1>home v2</h1>
-        <p>Excelling Your Professional Career</p>
-      </div>
-    </div>
+    // <div className="first-view">
+    //   <div className="first-view-content">
+    //     <h1>home v2</h1>
+    //     <p>Excelling Your Professional Career</p>
+    //   </div>
+    // </div>
+    <div>bbbers</div>
   );
 }
