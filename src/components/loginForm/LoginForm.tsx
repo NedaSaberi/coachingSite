16fd6caa316390d2fdd4e2f78c0c8e52dafa9118
Neda <neda1984saberi@gmail.com>
2023-11-08 21:56:33 +0100

    loginForm DONE

--- src/components/loginForm/LoginForm.tsx
+++ src/components/loginForm/LoginForm.tsx
@@ -8,35 +8,71 @@ export default function LoginForm() {
   const togglePasswordVisibility= ()=>{
     setShowPassword(!showPassword);
   }
+
+  const [user, setUser]= useState('');
+  const [pass, setPass]= useState('');
+  const clearForm = ()=>{
+    setUser('');
+    setPass('')
+  }
+  const submitHandler= (e: React.FormEvent)=>{
+    e.preventDefault();
+    const loginInfo = {
+      user: {user},
+      password: {pass}
+    }
+    console.log(loginInfo);
+    clearForm();
+  }
+
   return (
     <div className="formWrapper">
-      <h3 className='mb-5'>Log in</h3>
-      <form className="form" action="">
+      <h3 className="mb-5">Log in</h3>
+      <form className="form" action="" onSubmit={submitHandler}>
         <div className="input-group">
           <label htmlFor="user">
             Username or email address<span className="text-danger">*</span>
           </label>
-          <input id="user" type="text"/>
+          <input
+            id={styles.user}
+            value={user}
+            type="text"
+            onChange={(e) => setUser(e.target.value)}
+          />
         </div>
         <div className="input-group">
-            <label htmlFor="pass">
+          <label htmlFor="pass">
             Password<span className="text-danger">*</span>
-            </label>
-            <div className="password-input">
-              <input id="pass" type={showPassword ? 'text' : 'password'}/>
-              <span className="toggle-password" onClick={togglePasswordVisibility}>
-                {showPassword ? <i className="fas fa-eye"></i> : <i className="fas fa-eye-slash"></i>}
-              </span>
-            </div>
+          </label>
+          <div className="password-input">
+            <input
+              type={showPassword ? "text" : "password"}
+              id={styles.pass}
+              value={pass}
+              onChange={(e) => setPass(e.target.value)}
+            />
+            <span
+              className="toggle-password"
+              onClick={togglePasswordVisibility}
+            >
+              {showPassword ? (
+                <i className="fas fa-eye"></i>
+              ) : (
+                <i className="fas fa-eye-slash"></i>
+              )}
+            </span>
+          </div>
         </div>
         <div className="submit-group d-block my-3">
-            <button className={`${styles.shopBtn} bg-orange text-white`}>LOG IN</button>
-            <input type="checkbox" />
-            <span className='m-1'>
-              Remember me
-            </span>
+          <button className={`${styles.shopBtn} bg-orange text-white`}>
+            LOG IN
+          </button>
+          <input type="checkbox" />
+          <span className="m-1">Remember me</span>
         </div>
-        <Link to='/lost-password' className='text-orange link'>Lost your password?</Link>
+        <Link to="/lost-password" className="text-orange link">
+          Lost your password?
+        </Link>
       </form>
     </div>
   );
--- src/components/lostPassForm/LostPassForm.tsx
+++ src/components/lostPassForm/LostPassForm.tsx
@@ -1,15 +1,25 @@
 import styles from './LostPassForm.module.css';
+import { useState } from "react";
 
 export default function LostPassForm() {
+  const [user, setUser]= useState('');
+
+  const submitHandler = (e)=>{
+    e.preventDefault();
+    const lostInfo = {
+      user: {user}
+    }
+    console.log(lostInfo);
+  }
   return (
     <div className={`${styles.formWrapper} row`}>
-      <form className={`${styles.lostPassForm} col-12 col-lg-7`}>
+      <form className={`${styles.lostPassForm} col-12 col-lg-7`} onSubmit={submitHandler}>
         <p>
           Lost your password? Please enter your username or email address. You
           will receive a link to create a new password via email.
         </p>
         <div className="mb-2">Username or email</div>
-        <input className="col-12 col-md-6" type="text" />
+        <input className="col-12 col-md-6" type="text" value={user} onChange={(e) => setUser(e.target.value)}/>
         <button className={`${styles.resetPass} bg-orange`}>RESET PASSWORD</button>
       </form>
     </div>
